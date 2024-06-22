ee969722bf445fbaa600b100743e512833454ea6
softwareNinja-888 <mlandvodlamini03@gmail.com>
2024-06-22 18:31:02 +0200

    Add basic functionality to search feature

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = "wiki"
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("<str:title>", views.direct, name="direct")
+    path("", views.index, name="index"),    
+    # path("display/", views.display, name="display"),
+    path("<str:title>", views.direct, name="direct"),
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,26 +1,72 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse,HttpResponseRedirect
+from django.urls import reverse
+from django import forms
 from . import util
+
+# class newForm(forms.Form):
+#     items = forms.CharField(label=False,placeholder="Search Encyclopedia")
 
 
 def index(request):
+    list = util.list_entries()
+
+    if  request.method == "POST":
+        search = request.POST["q"]
+        if inList(search,list):
+            print(search)
+            return HttpResponseRedirect(reverse("wiki:direct", args=[f"{search}"]))
+        else:
+            return render(request,"encyclopedia/error.html",{
+                "error": search
+            })
+    
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
 def direct(request, title):
+    # GET METHOD:
     list = util.list_entries()
+    titleCaps = title.upper()
+    titleLow = title.lower()
     listCaps = []
     for n in list:
         listCaps.append(n.upper())
-
-    if title.upper() in listCaps:
-        index = listCaps.index(f"{title.upper()}")
-        print(title.lower())
-        return render(request, f"encyclopedia/{title.lower()}.html", {
+    
+    if inList(title,list):
+        index = listCaps.index(f"{titleCaps}")
+        return render(request, f"encyclopedia/{titleLow}.html", {
             "content": util.get_entry(list[index])
         })
     else:
         return render(request, "encyclopedia/error.html",{
             "error": title
         })
+
+# def display(request):
+#     # POST METHOD:
+
+#     if request.method == "POST":
+#         search = request.POST["q"]
+#         # IF (search in list):
+#             #redirect to relavant page
+#         #else if (search not in list):
+#             #redirect to search page (alternative page)
+#             #click on the list for redirect
+
+
+
+#         return HttpResponse(search)
+    
+def inList(n,list):
+    name = n.upper()
+    listCaps = []
+
+    for i in list:
+        listCaps.append(i.upper())
+    
+    if name in listCaps:
+        return True
+    else:
+        return False
