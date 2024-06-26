8c5041ad1ecc9d7166f8c98a4f02444ea238d120
softwareNinja-888 <mlandvodlamini03@gmail.com>
2024-06-26 16:25:41 +0200

    Modify create new page feature

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -4,12 +4,19 @@ from django.urls import reverse
 from django import forms
 from . import util
 
+
+import re
+
+from django.core.files.base import ContentFile
+from django.core.files.storage import default_storage
+
 class newForm(forms.Form):
-    title = forms.CharField(label="Title",widget=forms.TextInput(attrs={'class' : 'in'}))
+    title = forms.CharField(label="Title",widget=forms.TextInput(attrs={'class' : 'in', 'autofocus':'True', 'minlength' : '1'}))
     textarea = forms.CharField(widget=forms.Textarea(attrs={ 'class' : 'text','class' : 'in'}))
     
+list = util.list_entries()
+
 def index(request):
-    list = util.list_entries()
     subList = []
 
     # POST METHOD:
@@ -35,7 +42,6 @@ def index(request):
 
 def direct(request, title):
 
-    list = util.list_entries()
     titleCaps = title.upper()
     titleLow = title.lower()
     listCaps = []
@@ -53,11 +59,25 @@ def direct(request, title):
         })
     
 def new(request):
+
+    if request.method == "POST":
+        title = request.POST['title'].strip()
+        content = request.POST['textarea']
+
+        if inList(title,list):
+            print("Error Message/ Error page")
+        else:
+            util.save_entry(title,content)
+            save_entryHtml(title, content)
+            return HttpResponseRedirect(reverse("wiki:direct", args=[f"{title}"]))
+        
     return render(request,"encyclopedia/new.html", {
         "form": newForm()
     })
 
+# HELPER FUNCTIONS
 def inList(n,list):
+
     name = n.upper()
     listCaps = []
 
@@ -68,3 +88,10 @@ def inList(n,list):
         return True
     else:
         return False
+    
+def save_entryHtml(title, content):
+    
+    filename = f"encyclopedia/templates/encyclopedia/{title}.html"
+    if default_storage.exists(filename):
+        default_storage.delete(filename)
+    default_storage.save(filename, ContentFile(content))
